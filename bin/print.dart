6590378ebfe035783fbd08677f1ec6f8fe94ab99
nnn <maxkambekxolbekov515@gmail.com>
2022-07-17 09:12:46 -0700

    print01

---
+++ bin/print.dart
@@ -0,0 +1,24 @@
+void main (){
+    print ('   000"MAXKAMBEK FARM"');
+    print ('Kassir 1');
+    print ('sana 17.07.2022 10:55:22');
+    print ('stir:');
+    print ('1. senepar');
+    print ('        (10/100)x70 000.00=7000.00');
+    print ('         shu jumladan QQS 0 % 0.00');
+    print ('           MKIX: 03004001002000000');
+    print ('             sht kod 8901718020248');
+    print ('JAMI:                    =7 000,00');
+    print ('jami QQS                      0.00');
+    print ('NAQT                      7 000.00');
+    print ('BANK KARTA                    0.00');
+    print ('JAMI                      7 000.00');
+    print ('Virtual kassa S/R FOM 1234567890');
+    print ('FM raqam 1234567890');
+    print ('chek No:18974');
+    print ('fiskal belgi:234625044330');
+    print ('        SOTILGAN TOVAR');
+    print ('   ALMASHTIRILMAYDI VA QAYTARIB');
+    print ('             OLINMAYDI');
+    print ('    XARIDINGIZ UCHUM RAXMAT!!!');
+}
